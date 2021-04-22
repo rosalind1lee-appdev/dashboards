53b643b0675a9ec1daca0915f51a8ee9a3689423
rosalind1lee <rlee90@chicagobooth.edu>
2021-04-22 18:17:56 +0000

    Forex works All pages work

--- app/controllers/fx_controller.rb
+++ app/controllers/fx_controller.rb
@@ -6,4 +6,26 @@ class FxController < ApplicationController
     @array_of_symbols = @symbols_hash.keys
     render({ :template => "fx_templates/fx.html.erb" })
   end
+
+  def ind_rates
+    @sign = params.fetch("the_symbol")
+
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+    @array_of_symbols = @symbols_hash.keys
+
+    render({ :template => "fx_templates/ind_fx.html.erb" })
+  end
+
+  def fromto_rates
+    @sign1 = params.fetch("sym1")
+    @sign2 = params.fetch("sym2")
+
+    @raw_data = open("https://api.exchangerate.host/convert?from=#{@sign1}&to=#{@sign2}").read
+    @parsed_data = JSON.parse(@raw_data)
+    @output = @parsed_data.fetch("result")
+
+    render({ :template => "fx_templates/fromto_fx.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   #get("/", { :controller => "application", :action => "dashboard"})
   get("/forex", { :controller => "fx", :action => "rates"})
+  get("/forex/:the_symbol", { :controller => "fx", :action => "ind_rates" })
+  get("/forex/:sym1/:sym2", { :controller => "fx", :action => "fromto_rates" })
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
